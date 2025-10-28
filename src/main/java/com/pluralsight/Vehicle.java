087d8287e8f6b4bd8291ed24209fb40a3ef8a02e
YairVera <yairvera44@gmail.com>
2025-10-28 10:47:32 -0500

    create vehicle class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,52 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Vehicle {
+
+        private String color;
+        private int numberOfPassengers;
+        private int cargoCapacity;
+        private int fuelCapacity;
+        private ArrayList<Integer> wheels = new ArrayList<>();
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public ArrayList<Integer> getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(ArrayList<Integer> wheels) {
+        this.wheels = wheels;
+    }
 }
